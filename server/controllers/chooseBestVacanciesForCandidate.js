c2f0165f12689fe90a980feead6d990772a61ebf
IlarionHalushka <hilarion.galushka@gmail.com>
2019-01-27 13:52:07 +0200

    handle case when no skills were specified in /getBestVacancies req body

--- server/controllers/chooseBestVacanciesForCandidate.js
+++ server/controllers/chooseBestVacanciesForCandidate.js
@@ -13,10 +13,36 @@ const getBestVacancies = async function getBestVacancies({ skills, citiesIds, co
       companyId: { $in: companiesIds },
     };
   }
-  const vacancies = await Vacancy.find(query);
+
+  const searchResults = [];
+  let vacancies;
+
+  // if no skills were provided in request body return any 20 Vacancies
+  if (!skills) {
+    vacancies = await Vacancy.find(query).limit(20);
+
+    for (let i = 0; i < vacancies.length; i++) {
+      const [city, company] = await Promise.all([
+        City.findOne({ _id: vacancies[i].cityId }, { name: 1, externalId: 1 }),
+        Company.findOne({ _id: vacancies[i].companyId }, { name: 1, externalId: 1 }),
+      ]);
+      searchResults.push({
+        vacancyId: vacancies[i].externalId,
+        vacancyName: vacancies[i].name,
+        companyId: vacancies[i].companyId,
+        companyExternalId: company.externalId,
+        cityId: vacancies[i].cityId,
+        companyName: company.name,
+        cityName: city.name,
+      });
+    }
+
+    return searchResults;
+  }
+
+  vacancies = await Vacancy.find(query);
 
   // prepare stringWithSkillsSeparatedByCommas make it an array
-  let counters = [];
   for (let i = 0; i < vacancies.length; i++) {
     let counter = 0;
     for (let j = 0; j < vacancies[i].requirements.length; j++) {
@@ -37,7 +63,7 @@ const getBestVacancies = async function getBestVacancies({ skills, citiesIds, co
         City.findOne({ _id: vacancies[i].cityId }, { name: 1, externalId: 1 }),
         Company.findOne({ _id: vacancies[i].companyId }, { name: 1, externalId: 1 }),
       ]);
-      counters.push({
+      searchResults.push({
         vacancyId: vacancies[i].externalId,
         vacancyName: vacancies[i].name,
         companyId: vacancies[i].companyId,
@@ -49,9 +75,10 @@ const getBestVacancies = async function getBestVacancies({ skills, citiesIds, co
       });
     }
   }
-  counters = counters.sort(predicateBy('counter'));
-  counters.length = counters.length > 20 ? 20 : counters.length;
-  return counters;
+
+  searchResults.sort(predicateBy('counter'));
+  searchResults.length = searchResults.length > 20 ? 20 : searchResults.length;
+  return searchResults;
 };
 
 export default getBestVacancies;
