b9ce93471f5c353071fadcee595c57e5556f9c98
IlarionHalushka <hilarion.galushka@gmail.com>
2019-01-29 22:53:11 +0200

    improve algorithm for choosing best vacancies for candidate

--- server/controllers/chooseBestVacanciesForCandidate.js
+++ server/controllers/chooseBestVacanciesForCandidate.js
@@ -2,6 +2,8 @@ import { City, Company, Vacancy } from '../models';
 import { predicateBy } from '../utils/utils';
 
 const getBestVacancies = async function getBestVacancies({ skills, citiesIds, companiesIds }) {
+  console.time()
+
   const query = {};
   if (citiesIds) {
     query.$and = {
@@ -15,11 +17,10 @@ const getBestVacancies = async function getBestVacancies({ skills, citiesIds, co
   }
 
   const searchResults = [];
-  let vacancies;
 
   // if no skills were provided in request body return any 20 Vacancies
   if (!skills) {
-    vacancies = await Vacancy.find(query).limit(20);
+    const vacancies = await Vacancy.find(query).limit(20);
 
     for (let i = 0; i < vacancies.length; i++) {
       const [city, company] = await Promise.all([
@@ -40,24 +41,28 @@ const getBestVacancies = async function getBestVacancies({ skills, citiesIds, co
     return searchResults;
   }
 
-  vacancies = await Vacancy.find(query);
+  // if skills were provided
+  const vacancies = await Vacancy.find(query);
+
+  // prepare skills: toLowerCase and trim
+  for (let k = 0; k < skills.length; k++) {
+    skills[k].skill = skills[k].skill.toLowerCase().trim();
+  }
 
   // prepare stringWithSkillsSeparatedByCommas make it an array
   for (let i = 0; i < vacancies.length; i++) {
     let counter = 0;
     for (let j = 0; j < vacancies[i].requirements.length; j++) {
-      for (let k = 0; k < skills.length; k++) {
-        // get each requirement are remove symbols and put each word into array oneVacancyRequirementsArray
-        // stringWithOneRequirement example: "Good knowledge of JS." or "1+ years of experience!"
-        const stringWithOneRequirement = vacancies[i].requirements[j];
-        const oneVacancyRequirementsArray = stringWithOneRequirement.split(' ');
-        const stringWithSkillLowerCase = skills[k].skill.toLowerCase();
+      const stringWithOneRequirement = vacancies[i].requirements[j];
 
-        if (oneVacancyRequirementsArray.indexOf(stringWithSkillLowerCase.trim()) !== -1) {
+      for (let k = 0; k < skills.length; k++) {
+        if (stringWithOneRequirement.indexOf(skills[k].skill) !== -1) {
           counter += 1;
         }
       }
     }
+
+    // TODO: first sort results by predicate 'counter' and only after that query City, Company
     if (counter) {
       const [city, company] = await Promise.all([
         City.findOne({ _id: vacancies[i].cityId }, { name: 1, externalId: 1 }),
@@ -75,9 +80,11 @@ const getBestVacancies = async function getBestVacancies({ skills, citiesIds, co
       });
     }
   }
+  console.timeEnd()
 
   searchResults.sort(predicateBy('counter'));
   searchResults.length = searchResults.length > 20 ? 20 : searchResults.length;
+
   return searchResults;
 };
 
