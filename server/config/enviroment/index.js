6c474adac1b152c13a4710a4b0b664a2d559fa56
IlarionHalushka <hilarion.galushka@gmail.com>
2019-04-09 20:23:11 +0300

    add common env which will be 'merged' with other env configs

--- server/config/enviroment/index.js
+++ server/config/enviroment/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import developmentConfig from './development';
 import testConfig from './test';
+import common from './common';
 
 const detectEnvironment = (env) => ({
   'test': testConfig,
   'dev': developmentConfig,
 }[env]);
 
-export default detectEnvironment(process.env.NODE_ENV);
+export default _.assign(common, detectEnvironment(process.env.NODE_ENV));
