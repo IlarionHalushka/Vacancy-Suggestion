5e2c8fb38260857af56e2105e88525e1136bbc7e
IlarionHalushka <hilarion.galushka@gmail.com>
2019-04-22 21:43:44 +0300

    refactor translate utils

--- server/utils/translate.js
+++ server/utils/translate.js
@@ -3,19 +3,16 @@ import request from 'request-promise';
 import safeEval from 'safe-eval';
 import languages from './languages';
 
-const translate = async (text, opts = {}) => {
-  opts.from = opts.from || 'auto';
-  opts.to = opts.to || 'en';
+const translate = async (text, { from = 'auto', to = 'en' }) => {
+  from = languages.getCode(from);
+  to = languages.getCode(to);
 
-  opts.from = languages.getCode(opts.from);
-  opts.to = languages.getCode(opts.to);
-
-  let url = 'https://translate.googleapis.com/translate_a/single';
   const data = {
+    q: text,
+    sl: from,
+    tl: to,
+    hl: to,
     client: 'gtx',
-    sl: opts.from || 'auto',
-    tl: opts.to,
-    hl: opts.to,
     dt: 't',
     ie: 'UTF-8',
     oe: 'UTF-8',
@@ -23,9 +20,9 @@ const translate = async (text, opts = {}) => {
     ssel: 0,
     tsel: 0,
     kc: 7,
-    q: text,
   };
-  url = `${url}?${querystring.stringify(data)}`;
+
+  const url = `https://translate.googleapis.com/translate_a/single?${querystring.stringify(data)}`;
 
   const res = await request({
     url,
@@ -40,15 +37,16 @@ const translateWithTimeout = async (textToTranslate, timeout = 15000, opts = {})
     // translate doesn't allow to send more than 1000 symbols
     const textChunks = textToTranslate.match(/.{1,2500}/g);
     let textTranslated;
+
     for (const chunk of textChunks) {
       textTranslated += await translate(chunk, opts);
     }
 
     await new Promise(resolve => setTimeout(resolve, timeout));
+
     return textTranslated.toString();
   } catch (e) {
     console.error(e);
-    return 'no data';
   }
 };
 
